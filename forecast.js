d1cfe4dc9711bf5cd0adf162ebfadbe3801851bd
Kashish94 <kashishsinghlive@gmail.com>
2020-05-12 19:37:04 +0530

    forecast page designed

--- forecast.js
+++ forecast.js
@@ -21,10 +21,20 @@ function getForecast() {
             success: function(data) {
                 var table = "";
 
+                var header =
+                    "<h2 style='font-weight:bold; font-size:30px; margin-top:20px;'>Weather forecast for " +
+                    data.city.name +
+                    ", " +
+                    data.city.country +
+                    "</h2>";
+
                 for (var i = 0; i < data.list.length; i++) {
                     table += "<tr>";
 
-                    table += "<td>" + data.list[i].weather[0].icon + "</td>";
+                    table +=
+                        "<td><img src='http://openweathermap.org/img/wn/" +
+                        data.list[i].weather[0].icon +
+                        ".png'></td>";
                     table += "<td>" + data.list[i].weather[0].main + "</td>";
                     table += "<td>" + data.list[i].weather[0].description + "</td>";
                     table += "<td>" + data.list[i].main.temp_min + "&deg;C</td>";
@@ -38,6 +48,7 @@ function getForecast() {
                 }
 
                 $("#forecastWeather").html(table);
+                $("#header").html(header);
 
                 $("#city").val("");
                 $("#days").val("");
